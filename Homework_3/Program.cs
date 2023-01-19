c0837ba19fbfa490073d96eddbd0a992432ceac4
Asam <asamvp@mail.ru>
2023-01-19 12:16:58 +0500

    Доб 3 дз

---
+++ Homework_3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+int inputInt(string Msg)
+{
+    Console.WriteLine(Msg);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+int a = inputInt("Введите день недели");
+if (a > 0 && a < 8)
+{
+    if (a == 6 || a==7)
+    {
+        Console.WriteLine("Выходной, можно отдыхать...");
+    }
+    else { Console.WriteLine("Иди на работу"); }
+}
+else
+{
+    Console.WriteLine("Такого дня нет");
+}
+
